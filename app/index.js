c6a127fb3df9fbd63f01a820941efae9cf1ea9bb
Rupalshah17 <shah17rupal@gmail.com>
2023-07-03 22:55:50 +0530

    checked

--- app/index.js
+++ app/index.js
@@ -1,35 +1,31 @@
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
-const express = require('express');
-const Blockchain = require('../blockchain');
-const bodyParser = require('body-parser');
+const express = require("express");
+const Blockchain = require("../blockchain");
+const bodyParser = require("body-parser");
 const P2pserver = require("../p2p-server");
-const p2pserver = new P2pserver(Blockchain);
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 app.use(bodyParser.json());
 
-
 const blockchain = new Blockchain();
-
-app.get('/blocks', (req, res) => {
-    res.json(blockchain.chain);
+const p2pserver = new P2pserver(blockchain);
+app.get("/blocks", (req, res) => {
+  res.json(blockchain.chain);
+  console.log("blockchain", blockchain.chain);
 });
 
-app.post('/mine', (req, res) => {
-    const block = blockchain.addBlock(req.body.data);
-    console.log(`New block added: ${block.toString()}`);
+app.post("/mine", (req, res) => {
+  const block = blockchain.addBlock(req.body.data);
+  console.log(`New block added: ${block.toString()}`);
 
-
-    res.redirect('/blocks');
+  res.redirect("/blocks");
 });
 
 app.listen(HTTP_PORT, () => {
-    console.log(`listening on port ${HTTP_PORT}`);
-})
-
+  console.log(`listening on port ${HTTP_PORT}`);
+});
 
 p2pserver.listen();
 // this starts the server
-
